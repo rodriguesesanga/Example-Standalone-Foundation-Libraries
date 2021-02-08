a8ec317a1e6e9e0c6582d975282304d2ff56c3aa
Rodrigue Sesanga <rodrigue.sesanga@microej.com>
2021-02-09 00:57:10 +0100

    updates files

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,22 +1,21 @@
 def nodeInfo() {
   println "Node: '${env.NODE_NAME}'"
 }
-def failed_message(link_gif){
+def failed_message(file){
   body="""
   <!DOCTYPE html>
   <html>
   <body style='font-family: sans-serif;'>
   <h1 align="center">&#x1F6A8; &#x1F6A8; &#x1F6A8; Build <code style="color:blue">#${env.BUILD_NUMBER}</code> in <i><code>${env.JOB_NAME}</code></i> failed! &#x1F6A8;&#x1F6A8;&#x1F6A8 </h1>
   </br>
-  <h2 align="center">This failure comes from the running of that JOB on the label:  <i style="color: blue">Myparams</i><h2>
-  <p style="color: red" align="center"><strong>Click on the link below to find out cause and try to fix it </strong></p>
-  <p align="center">&#X1F517;<a href=${env.BUILD_URL}><code>${env.JOB_NAME}</code><code>#${env.BUILD_NUMBER}</code></a></p>
-
-  <p align="center"><img src="https://www.microej.com/wp-content/uploads/2019/02/Product_SDK_mascotsdk.png" height="400" width="400"></p>
+  <h2 align="center">This failure comes from the running of that JOB on the label:  <i style="color: blue">${NODE_NAME}</i><h2>
+  <p style="color: red" align="center">The following links send 404 error: ${file}</p>
+  <p style="color: red" align="center"><strong>Click on the link below to find out them </strong></p>
+  <p align="center">&#X1F517;<a href=${env.BUILD_URL}</a></p>
   </body>
   </html>
   """
-  emailext body: "${body}" ,mimeType: 'text/html',subject: "Build failed in Jenkins: ${env.JOB_NAME}, #${env.BUILD_NUMBER}-FAILURE", to: 'rodrigue.sesanga@microej.com'
+  emailext body: "${body}" ,mimeType: 'text/html',subject: "Test links failed on: ${env.JOB_NAME}, #${env.BUILD_NUMBER}-FAILURE", to: 'rodrigue.sesanga@microej.com'
 }
 
 try{
@@ -57,14 +56,16 @@ try{
 		  done'''
     }
     stage('Looking URLs error') {
-	def contentFile = readFile('error_url.txt')
-	println("${contentFile}")
+	def contentErrorFile = readFile('error_url.txt')
+	if (contentErrorFile != null){
+		println("${contentFile}")
+		currentBuild.result = "FAILURE"
+	}
 
     }
   }
  }
 }catch (e) {
   // In this case, the build failed
-  currentBuild.result = "FAILURE"
-  failed_message('https://media.giphy.com/media/h8ncU2Ps6b2zS/giphy.gif')
+  failed_message("${contentErrorFile}")
 }
