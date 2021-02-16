732c85a7b6217cf86f7c5b71e14e7d3e1f0e39b8
Rodrigue Sesanga <rodrigue.sesanga@microej.com>
2021-02-16 15:47:10 +0100

    Updates jenkinsfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -11,7 +11,7 @@ def error_url_message(file){
   <h2 align="center"><i><code style="color:blue">${env.JOB_NAME}</code></i></h2>
   </br>
   <h2 align="center">This error is due to: <h2>
-  <ul><li> <?php echo 'Links : ' .${file}. '<br>'; ?></li></ul>
+  <ul><li>${file}</li></ul>
   <p style="color: red" align="center"><strong>Click on the link below to open the build.</strong></p>
   <p align="center">&#X1F517;<a href=${env.BUILD_URL}><code>${env.JOB_NAME}</code><code>#${env.BUILD_NUMBER}</code></a></p>
   </body>
